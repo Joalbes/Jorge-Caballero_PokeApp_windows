617ba56e3b8b28d90033d3ff36c75ff9f83410fb
Jorge A Caballero <joalbes@gmail.com>
2023-04-13 09:34:17 -0500

    2023/04/13 9:35am

---
+++ components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-[18%] h-[1/2] bg-gray-200 flex flex-col justify-center items-center">
+        <img :src="url" >
+        <p>{{ nombre }}</p>
+    </div>
+</template>
+<script>
+export default{
+    props:{
+        nombre:{
+            type:String,
+            required:true
+        },
+        url:{
+            type:String,
+            required: true
+        }
+    }
+}</script>
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,42 @@
 <template>
-  <Tutorial/>
+  <div class="h-screen">
+
+    <div class="flex justify-center items-center py[1%]">
+      <p class="text-6xl font-bold">PokeDEx</p>
+    </div>
+
+    <div>
+      <input type="text" class="border-solid border-2 border-green-100 bg-green-50 mt-[2%]">
+    </div>
+
+    <div class="my-[2%] flex flex-wrap px-[5%] h-[80%]">
+      <Card v-for="i in posts" :key="i.name" :nombre="i.name" :url="i.sprites.other.dream_world.front_default" class="mx-[0.5%] my-[0.5%]"></Card>
+    </div>
+
+  </div>
 </template>
 
 <script>
+import { log } from 'console';
+
 export default {
-  name: 'IndexPage'
+  data() {
+    return {
+      posts: [] 
+    };
+  },
+  mounted() {
+    this.getInfo()
+  },
+  methods: {
+    async getInfo() {     
+      for (let index = 1; index <= 20; index++) {
+        let {data} = await this.$axios.get(`pokemon/${index}/`)
+        this.posts.push(data)                      
+      }   
+         
+    }   
+  }
 }
 </script>
+<style></style>
